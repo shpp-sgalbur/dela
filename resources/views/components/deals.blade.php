427a56f19f575946f32521ea390846c7dc15efbe
Galbur Sergey <gal_sergey@ukr.net>
2021-05-29 09:21:09 +0300

    Решил проблему отображения сообщения, когда список дел пуст.

--- resources/views/components/deals.blade.php
+++ resources/views/components/deals.blade.php
@@ -1,15 +1,13 @@
 <div class="w-auto m-2 border-solid border-4 border-light-blue-500">
-   
-    <!-- Well begun is half done. - Aristotle -->
-    @if(($deals)==null)
+     
+    @if($deals->total()==null)
         <div>
             Список дел в категории пуст. Чтобы добавить дело выберите "Добавить дело"
         </div>
     @else
     
         @foreach($deals as $deal)
-        <x-deal :deal="$deal" :category="$category"></x-deal>
-            
+            <x-deal :deal="$deal" :category="$category"></x-deal>            
         @endforeach
         {{$deals->links()}}
    
