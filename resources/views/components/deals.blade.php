16c9d935eeae393f23b0428248cb505171a7a22e
Galbur Sergey <gal_sergey@ukr.net>
2021-05-29 14:48:23 +0300

    Хочу отказаться от шаблона showCategory  и обходиться только шаблономdesktop

--- resources/views/components/deals.blade.php
+++ resources/views/components/deals.blade.php
@@ -1,16 +1,20 @@
-<div class="w-auto m-2 border-solid border-4 border-light-blue-500">
+
      
     @if($deals->total()==null)
         <div>
             Список дел в категории пуст. Чтобы добавить дело выберите "Добавить дело"
         </div>
     @else
-    
+        <div class="m-2 border-solid border-green-500 border-4 rounded-2xl box-border  bg-green-100">
+                        <div class=" m-2 text-3xl">
+                            Список дел категории "{{$category->category}}"
+                        </div>
+                                
+                   
         @foreach($deals as $deal)
             <x-deal :deal="$deal" :category="$category"></x-deal>            
         @endforeach
         {{$deals->links()}}
+         </div>
    
     @endif
-    
-</div>
