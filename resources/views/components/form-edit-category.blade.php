02ef7df1bcae855041331926e120b406bde44836
Galbur Sergey <gal_sergey@ukr.net>
2021-06-23 21:01:48 +0300

    ДобавиK название редактируемой категории в форму редактирования категории

--- resources/views/components/form-edit-category.blade.php
+++ resources/views/components/form-edit-category.blade.php
@@ -1,7 +1,7 @@
  
 <div class="m-2 border-solid border-green-500 border-4 rounded-2xl box-border h-40">
         
-    <h6 class=" m-2 text-3xl">Форма редактирования категории</h6>
+    <h6 class=" m-2 text-3xl">Форма редактирования категории <br><b>{{$currentcategory->category}}</b> </h6>
     <form action="{{route('category.update',['category'=>$currentcategory])}}" method="post" class=" m-2 p-2   ">
         @csrf
         @method('PUT')
