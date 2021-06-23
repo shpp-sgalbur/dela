566ebee0b090f26a460d4abd04cf40de40720475
Galbur Sergey <gal_sergey@ukr.net>
2021-06-23 21:09:45 +0300

    Измениk стиль текста в заголовке Список категорий

--- resources/views/components/categories.blade.php
+++ resources/views/components/categories.blade.php
@@ -1,11 +1,12 @@
 
 <div class="m-2 border-solid border-green-500 border-2  box-border">
     <div class="flex flex-row ">
-        <h6 class=" m-2">Список категорий</h6>
+        <h6 class=" m-2 p-2 text-2xl bg-green-200">Список категорий</h6>
         <form action="{{route('category.create')}}" class=" m-2">
             <input type="submit" value="+" class="border-2 border-green-500 border-solid m-1 p-1 rounded-lg bg-green-200 h-8 w-8 font-bold text-green-900"/>
         </form>
     </div>
+    <hr>
     
     
     @foreach($categories as $category)
