de8665cd5246e8ce845ec9541f7a054cc4cfe00f
Galbur Sergey <gal_sergey@ukr.net>
2021-09-18 21:59:18 +0300

    добавил посредник, проверяющий надо ли голосовать перед добавлением дела

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,10 +53,12 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
+        'check.votes' => \App\Http\Middleware\CheckVotes::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
